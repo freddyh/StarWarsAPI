741786c10a5928c8df62dee26855d2cc61084937
fhj <307890+freddyh@users.noreply.github.com>
2020-08-03 23:45:54 -0700

    test for people request

--- Sources/star-wars-api/star_wars_api.swift
+++ Sources/star-wars-api/star_wars_api.swift
@@ -1,3 +1,40 @@
-struct star_wars_api {
+import Combine
+import Foundation
+
+struct StarWarsAPI {
+    
+    struct PeopleWrapper: Codable {
+        let results: [Person]
+    }
+    
+    struct Person: Codable {
+        let name: String
+        let height: String
+        let mass: String
+        let homeWorld: String?
+        let films: [String]
+        let url: String
+        let created: String
+        let edited: String
+    }
+    
+    struct APIError: Error {
+        let reason: String
+    }
+    
     var text = "Hello, World!"
+    
+    var cancellables: Set<AnyCancellable> = []
+    
+    func peoplePublisher() -> AnyPublisher<[Person], APIError> {
+        let url = URL(string: "https://swapi.dev/api/people")!
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map({ $0.data })
+            .decode(type: PeopleWrapper.self, decoder: JSONDecoder())
+            .map({ $0.results })
+            .mapError({ (failure) -> APIError in
+                return APIError(reason: failure.localizedDescription)
+            })
+            .eraseToAnyPublisher()
+    }
 }
--- Tests/star-wars-apiTests/star_wars_apiTests.swift
+++ Tests/star-wars-apiTests/star_wars_apiTests.swift
@@ -1,15 +1,26 @@
 import XCTest
+import Combine
 @testable import star_wars_api
 
 final class star_wars_apiTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(star_wars_api().text, "Hello, World!")
+    var cancellabes: Set<AnyCancellable> = []
+    
+    func testPeople() {
+        let exp = expectation(description: "Completion block called")
+        StarWarsAPI().peoplePublisher()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error): XCTFail(error.localizedDescription)
+                default: break
+                }
+            }) { (people) in
+                exp.fulfill()
+                XCTAssert(people.count > 0)
+            }.store(in: &cancellabes)
+        waitForExpectations(timeout: 8, handler: nil)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testPeople", testPeople),
     ]
 }
